2668a727a2fff296e535e24233224a33371ca8c8
Eric - Mobile <ecarpen905@gmail.com>
2018-06-30 00:11:50 -0700

    First commit, a bit late. Program to gather invoice data out of mFiles and process into format to upload into Yardi. Currently can open vault and search for invoices, need to pull data next.

--- DavisInvoice/Form1.Designer.cs
+++ DavisInvoice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DavisInvoice
 {
-    partial class Form1
+    partial class DavisInvoice
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DavisInvoice));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Logo, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.39024F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.60976F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(394, 335);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(394, 335);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Logo
+            // 
+            this.Logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Logo.BackgroundImage")));
+            this.Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanel1.SetColumnSpan(this.Logo, 2);
+            this.Logo.InitialImage = null;
+            this.Logo.Location = new System.Drawing.Point(3, 3);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(794, 103);
+            this.Logo.TabIndex = 2;
+            this.Logo.TabStop = false;
+            // 
+            // DavisInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DavisInvoice";
+            this.Text = "Davis Invoice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox Logo;
     }
 }
 
--- DavisInvoice/Form1.cs
+++ DavisInvoice/Form1.cs
@@ -7,14 +7,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MFilesAPI;
 
 namespace DavisInvoice
 {
-    public partial class Form1 : Form
+    public partial class DavisInvoice : Form
     {
-        public Form1()
+
+        //Settings until a proper setting menu is created
+        static int invoiceWorkflow = 108;
+        static int stateAccounting = 144;
+        static int stateProcessing = 145;
+        static int stateComplete = 146;
+        static int documentType = 0;
+
+        public DavisInvoice()
         {
             InitializeComponent();
+            //connect to mFiles
+            var mFilesApp = new MFilesClientApplication();
+
+            //open vault
+            var vaultConnect = new VaultConnection();
+            vaultConnect = mFilesApp.GetVaultConnection("My Vault");
+
+            var currVault = new Vault();
+            currVault = vaultConnect.BindToVault(this.Handle, true, false);
+
+            //export
+            button1.Click += delegate (object sender, EventArgs e) { button1_Click(sender, e, currVault, mFilesApp); };
+
+            //pull up invoices
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        //Export Button
+        private void button1_Click(object sender, EventArgs e, Vault currVault, MFilesClientApplication mFilesApp)
+        {
+            //MessageBox.Show(currVault.Name);
+
+            //pull invoices using search conditions
+            var searchConditions = new SearchConditions();
+
+
+            
+          
+
+            //is it not deleted
+            var isNotDeleted = new SearchCondition();
+            isNotDeleted.Expression.DataStatusValueType = MFStatusType.MFStatusTypeDeleted;
+            isNotDeleted.Expression.DataStatusValueDataFunction = MFDataFunction.MFDataFunctionNoOp;
+            isNotDeleted.ConditionType = MFConditionType.MFConditionTypeNotEqual;
+            isNotDeleted.TypedValue.SetValue(MFDataType.MFDatatypeBoolean, true);
+            searchConditions.Add(-1, isNotDeleted);
+
+            //is it part of the Invoice workflow
+            var isInvoice = new SearchCondition();
+            isInvoice.Expression.DataPropertyValuePropertyDef = (int)MFBuiltInPropertyDef.MFBuiltInPropertyDefWorkflow;
+            isInvoice.ConditionType = MFConditionType.MFConditionTypeEqual;
+            isInvoice.TypedValue.SetValue(MFDataType.MFDatatypeLookup, invoiceWorkflow);
+            searchConditions.Add(-1, isInvoice);
+
+            //is it in the accounting state
+            var isAccounting = new SearchCondition();
+            isAccounting.Expression.DataPropertyValuePropertyDef = (int)MFBuiltInPropertyDef.MFBuiltInPropertyDefState;
+            isAccounting.ConditionType = MFConditionType.MFConditionTypeEqual;
+            isAccounting.TypedValue.SetValue(MFDataType.MFDatatypeLookup, stateAccounting);
+            searchConditions.Add(-1, isAccounting);
+
+            var invoices = currVault.ObjectSearchOperations.SearchForObjectsByConditions(searchConditions, MFSearchFlags.MFSearchFlagNone, false);
+
+            foreach (ObjectVersion invoice in invoices)
+            {
+                var objtype = default(ObjType);
+                objtype = currVault.ObjectTypeOperations.GetObjectType(invoice.ObjVer.Type);
+                MessageBox.Show(objtype.NameSingular);
+            }
+
+            //loop through invoices collecting at import workflow state
+
+            //add data to xml object
+
+            //change workflow state 
+
+            //End Loop
+
+            //combine xaml objects
+
+            //save export file
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            //loop through invoices collecting at import workflow state
+
+            //add data to xml object
+
+            //change workflow state 
+
+            //End Loop
+
+            //combine xaml objects
+
+            //save export file
+        }
+
+        //import button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //open XML file
+
+            //loop through items
+
+            //find mFiles object with matching invoice number
+
+            //add check number to mFiles Object
+
+            //change state to complete
+
+            //End Loop
         }
     }
 }
